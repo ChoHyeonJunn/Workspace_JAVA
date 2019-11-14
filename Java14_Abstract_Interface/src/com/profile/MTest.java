2630dcb7ca2d1a87dfa9b189723e04e7ccc8f083
ancsbbc@naver.com <53424490+ChoHyeonJunn@users.noreply.github.com>
2019-11-14 21:16:28 +0900

    2019-11-14 modified

--- Java14_Abstract_Interface/src/com/profile/MTest.java
+++ Java14_Abstract_Interface/src/com/profile/MTest.java
@@ -4,9 +4,7 @@ public class MTest {
 
 	/*
 	 * Profile 과 Display, Job 을 상속받는
-	 * MyProfile 클래스를 만들자.
-	 * 
-	 * 
+	 * MyProfile 클래스를 만들자. 
 	 */
 	public static void main(String[] args) {
 		MyProfile my = new MyProfile("kh", "010-1234-5678");
--- Java14_Abstract_Interface/src/com/profile/MyProfile.java
+++ Java14_Abstract_Interface/src/com/profile/MyProfile.java
@@ -5,17 +5,17 @@ public class MyProfile extends Profile implements Job, Display {
 	private String loc;
 	
 	public MyProfile(String name, String phone) {
-		super(name, phone);
 		// TODO Auto-generated constructor stub
+		super(name, phone);
 	}
 
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
-
 		super.prn();
 		System.out.println("회사주소 : " + loc);
-		System.out.println("회사직종 : " + JOB_ID);
+		System.out.println("회사직종 : " + JOB_ID);	
+		//super.JOB_ID -> X : 부모 클래스만 super가능, JOB_ID 를 가지고 있는 Job 은 interface 이다.
 	}
 
 	@Override
